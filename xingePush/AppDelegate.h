85859e865ed9cf5df1c182f35aaa2ea53396fff2
fj-zkyc <912796116@qq.com>
2017-04-12 11:55:02 +0800

    Initial Commit

---
+++ xingePush/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xingePush
+//
+//  Created by fj-zkyc on 17/4/12.
+//  Copyright © 2017年 PLwales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xingePush/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xingePush
+//
+//  Created by fj-zkyc on 17/4/12.
+//  Copyright © 2017年 PLwales. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
