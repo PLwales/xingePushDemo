87e977908d08cec6d5753271ca4c9b45f25631b2
fj-zkyc <912796116@qq.com>
2017-04-25 09:28:50 +0800

    信鸽推送建议demo

--- xingePush/AppDelegate.m
+++ xingePush/AppDelegate.m
@@ -7,6 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import "XGSetting.h"
+#import "XGPush.h"
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+
+#import <UserNotifications/UserNotifications.h>
+@interface AppDelegate() <UNUserNotificationCenterDelegate>
+@end
+#endif
+
 
 @interface AppDelegate ()
 
@@ -16,7 +25,24 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    [[XGSetting getInstance] enableDebug:YES];
+    [XGPush startApp:2200256456 appKey:@"IUUB7J4564UN"];
+    
+    [XGPush isPushOn:^(BOOL isPushOn) {
+        NSLog(@"[XGDemo] Push Is %@", isPushOn ? @"ON" : @"OFF");
+    }];
+    
+    [self registerAPNS];
+    
+    [XGPush handleLaunching:launchOptions successCallback:^{
+        NSLog(@"[XGDemo] Handle launching success");
+    } errorCallback:^{
+        NSLog(@"[XGDemo] Handle launching error");
+    }];
+
+    
+    
+    
     return YES;
 }
 
@@ -39,12 +65,145 @@
 
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+
+        
+        // 清除图标数字
+        
+        application.applicationIconBadgeNumber = 0;
+    
 }
 
 
 - (void)applicationWillTerminate:(UIApplication *)application {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+    
+    NSString *deviceTokenStr = [XGPush registerDevice:deviceToken account:@"18727978051" successCallback:^{
+        NSLog(@"[XGDemo] register push success");
+    } errorCallback:^{
+        NSLog(@"[XGDemo] register push error");
+    }];
+    NSLog(@"[XGDemo] device token is %@", deviceTokenStr);
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+    NSLog(@"[XGDemo] register APNS fail.\n[XGDemo] reason : %@", error);
+}
+
+
+/**
+ 收到通知的回调
+ 
+ @param application  UIApplication 实例
+ @param userInfo 推送时指定的参数
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+    NSLog(@"[XGDemo] receive Notification");
+    [XGPush handleReceiveNotification:userInfo
+                      successCallback:^{
+                          NSLog(@"[XGDemo] Handle receive success");
+                      } errorCallback:^{
+                          NSLog(@"[XGDemo] Handle receive error");
+                      }];
+}
+
+
+/**
+ 收到静默推送的回调
+ 
+ @param application  UIApplication 实例
+ @param userInfo 推送时指定的参数
+ @param completionHandler 完成回调
+ */
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler {
+    NSLog(@"[XGDemo] receive slient Notification");
+    NSLog(@"[XGDemo] userinfo %@", userInfo);
+    [XGPush handleReceiveNotification:userInfo
+                      successCallback:^{
+                          NSLog(@"[XGDemo] Handle receive success");
+                      } errorCallback:^{
+                          NSLog(@"[XGDemo] Handle receive error");
+                      }];
+    
+    completionHandler(UIBackgroundFetchResultNewData);
+}
+
+// iOS 10 新增 API
+// iOS 10 会走新 API, iOS 10 以前会走到老 API
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// App 用户点击通知的回调
+// 无论本地推送还是远程推送都会走这个回调
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void(^)())completionHandler {
+    NSLog(@"[XGDemo] click notification");
+    [XGPush handleReceiveNotification:response.notification.request.content.userInfo
+                      successCallback:^{
+                          NSLog(@"[XGDemo] Handle receive success");
+          //  [UIApplicationsharedApplication].applicationIconBadgeNumber = 0;
+                          
+
+                      } errorCallback:^{
+                          NSLog(@"[XGDemo] Handle receive error");
+                      }];
+    
+    completionHandler();
+}
+
+// App 在前台弹通知需要调用这个接口
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions options))completionHandler {
+    
+    
+    completionHandler(UNNotificationPresentationOptionBadge | UNNotificationPresentationOptionSound | UNNotificationPresentationOptionAlert);
+}
+#endif
+
+- (void)registerAPNS {
+    float sysVer = [[[UIDevice currentDevice] systemVersion] floatValue];
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    if (sysVer >= 10) {
+        // iOS 10
+        [self registerPush10];
+    } else if (sysVer >= 8) {
+        // iOS 8-9
+        [self registerPush8to9];
+    } else {
+        // before iOS 8
+        [self registerPushBefore8];
+    }
+#else
+    if (sysVer < 8) {
+        // before iOS 8
+        [self registerPushBefore8];
+    } else {
+        // iOS 8-9
+        [self registerPush8to9];
+    }
+#endif
+}
+
+- (void)registerPush10{
+#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
+    center.delegate = self;
+    
+    
+    [center requestAuthorizationWithOptions:UNAuthorizationOptionBadge | UNAuthorizationOptionSound | UNAuthorizationOptionAlert completionHandler:^(BOOL granted, NSError * _Nullable error) {
+        if (granted) {
+        }
+    }];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+#endif
+}
+
+- (void)registerPush8to9{
+    UIUserNotificationType types = UIUserNotificationTypeBadge | UIUserNotificationTypeSound | UIUserNotificationTypeAlert;
+    UIUserNotificationSettings *mySettings = [UIUserNotificationSettings settingsForTypes:types categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:mySettings];
+    [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
+
+- (void)registerPushBefore8{
+    [[UIApplication sharedApplication] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert | UIRemoteNotificationTypeBadge | UIRemoteNotificationTypeSound)];
 }
 
 
--- xingePush/ViewController.m
+++ xingePush/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "XGPush.h"
+
 
 @interface ViewController ()
+@property (nonatomic, strong) UIAlertController *alertCtr;
 
 @end
 
@@ -16,9 +19,83 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    if (!self.alertCtr) {
+        self.alertCtr = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *action = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        }];
+        [self.alertCtr addAction:action];
+    }
 }
 
+- (IBAction)onsetTag:(UIButton *)sender {
+    [self setTag:@"myTag"];
+
+}
+
+- (IBAction)delTag:(UIButton *)sender {
+    [self ondelTag:@"myTag"];
+
+}
+
+
+
+- (IBAction)onsetAccount:(UIButton *)sender {
+    [self setAccount:@"myAccount"];
+
+}
+
+- (IBAction)delAccount:(UIButton *)sender {
+    
+    [self delAccount];
+}
+
+- (void)setTag:(NSString *)tag {
+    [XGPush setTag:@"myTag" successCallback:^{
+        NSLog(@"[XGDemo] Set tag success");
+        [self.alertCtr setMessage:@"Set tag success"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    } errorCallback:^{
+        NSLog(@"[XGDemo] Set tag error");
+        [self.alertCtr setMessage:@"Set tag error"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    }];
+}
+
+- (void)ondelTag:(NSString *)tag  {
+    [XGPush delTag:@"myTag" successCallback:^{
+        NSLog(@"[XGDemo] Del tag success");
+        [self.alertCtr setMessage:@"Del tag success"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    } errorCallback:^{
+        NSLog(@"[XGDemo] Del tag error");
+        [self.alertCtr setMessage:@"Del tag error"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    }];
+}
+
+- (void)setAccount:(NSString *)account {
+    [XGPush setAccount:@"myAccount" successCallback:^{
+        NSLog(@"[XGDemo] Set account success");
+        [self.alertCtr setMessage:@"Set account success"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    } errorCallback:^{
+        NSLog(@"[XGDemo] Set account error");
+        [self.alertCtr setMessage:@"Set account error"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    }];
+}
+
+- (void)delAccount {
+    [XGPush delAccount:^{
+        NSLog(@"[XGDemo] Del account success");
+        [self.alertCtr setMessage:@"Del account success"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    } errorCallback:^{
+        NSLog(@"[XGDemo] Del account error");
+        [self.alertCtr setMessage:@"Del account error"];
+        [self presentViewController:self.alertCtr animated:YES completion:nil];
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
